96a100aa465718a91e3dd7de7cedabe322baf53f
mrbobselfridge <selfridge@gmail.com>
2024-03-19 10:57:36 -0400

    asd

---
+++ UpdateTableNames.sql
@@ -0,0 +1,170 @@
+USE gdavis42;
+ALTER TABLE `categories` RENAME TO `Categories`;
+ALTER TABLE `customers` RENAME TO `Customers`;
+ALTER TABLE `order details` RENAME TO `OrderDetails`;
+ALTER TABLE `orders` RENAME TO `Orders`;
+ALTER TABLE `products` RENAME TO `Products`;
+ALTER TABLE `shippers` RENAME TO `Shippers`;
+ALTER TABLE `suppliers` RENAME TO `Suppliers`;
+
+USE dgarcia84;
+ALTER TABLE `categories` RENAME TO `Categories`;
+ALTER TABLE `customers` RENAME TO `Customers`;
+ALTER TABLE `order details` RENAME TO `OrderDetails`;
+ALTER TABLE `orders` RENAME TO `Orders`;
+ALTER TABLE `products` RENAME TO `Products`;
+ALTER TABLE `shippers` RENAME TO `Shippers`;
+ALTER TABLE `suppliers` RENAME TO `Suppliers`;
+
+USE lgates99;
+ALTER TABLE `categories` RENAME TO `Categories`;
+ALTER TABLE `customers` RENAME TO `Customers`;
+ALTER TABLE `order details` RENAME TO `OrderDetails`;
+ALTER TABLE `orders` RENAME TO `Orders`;
+ALTER TABLE `products` RENAME TO `Products`;
+ALTER TABLE `shippers` RENAME TO `Shippers`;
+ALTER TABLE `suppliers` RENAME TO `Suppliers`;
+
+USE dhirschbiel41;
+ALTER TABLE `categories` RENAME TO `Categories`;
+ALTER TABLE `customers` RENAME TO `Customers`;
+ALTER TABLE `order details` RENAME TO `OrderDetails`;
+ALTER TABLE `orders` RENAME TO `Orders`;
+ALTER TABLE `products` RENAME TO `Products`;
+ALTER TABLE `shippers` RENAME TO `Shippers`;
+ALTER TABLE `suppliers` RENAME TO `Suppliers`;
+
+USE dhollopeter96;
+ALTER TABLE `categories` RENAME TO `Categories`;
+ALTER TABLE `customers` RENAME TO `Customers`;
+ALTER TABLE `order details` RENAME TO `OrderDetails`;
+ALTER TABLE `orders` RENAME TO `Orders`;
+ALTER TABLE `products` RENAME TO `Products`;
+ALTER TABLE `shippers` RENAME TO `Shippers`;
+ALTER TABLE `suppliers` RENAME TO `Suppliers`;
+
+USE creigh45;
+ALTER TABLE `categories` RENAME TO `Categories`;
+ALTER TABLE `customers` RENAME TO `Customers`;
+ALTER TABLE `order details` RENAME TO `OrderDetails`;
+ALTER TABLE `orders` RENAME TO `Orders`;
+ALTER TABLE `products` RENAME TO `Products`;
+ALTER TABLE `shippers` RENAME TO `Shippers`;
+ALTER TABLE `suppliers` RENAME TO `Suppliers`;
+
+USE dringler83;
+ALTER TABLE `categories` RENAME TO `Categories`;
+ALTER TABLE `customers` RENAME TO `Customers`;
+ALTER TABLE `order details` RENAME TO `OrderDetails`;
+ALTER TABLE `orders` RENAME TO `Orders`;
+ALTER TABLE `products` RENAME TO `Products`;
+ALTER TABLE `shippers` RENAME TO `Shippers`;
+ALTER TABLE `suppliers` RENAME TO `Suppliers`;
+
+USE gdavis42;
+ALTER TABLE `categories` RENAME TO `Categories`;
+ALTER TABLE `customers` RENAME TO `Customers`;
+ALTER TABLE `order details` RENAME TO `OrderDetails`;
+ALTER TABLE `orders` RENAME TO `Orders`;
+ALTER TABLE `products` RENAME TO `Products`;
+ALTER TABLE `shippers` RENAME TO `Shippers`;
+ALTER TABLE `suppliers` RENAME TO `Suppliers`;
+
+USE tryan85;
+ALTER TABLE `categories` RENAME TO `Categories`;
+ALTER TABLE `customers` RENAME TO `Customers`;
+ALTER TABLE `order details` RENAME TO `OrderDetails`;
+ALTER TABLE `orders` RENAME TO `Orders`;
+ALTER TABLE `products` RENAME TO `Products`;
+ALTER TABLE `shippers` RENAME TO `Shippers`;
+ALTER TABLE `suppliers` RENAME TO `Suppliers`;
+
+USE sseltner48;
+ALTER TABLE `categories` RENAME TO `Categories`;
+ALTER TABLE `customers` RENAME TO `Customers`;
+ALTER TABLE `order details` RENAME TO `OrderDetails`;
+ALTER TABLE `orders` RENAME TO `Orders`;
+ALTER TABLE `products` RENAME TO `Products`;
+ALTER TABLE `shippers` RENAME TO `Shippers`;
+ALTER TABLE `suppliers` RENAME TO `Suppliers`;
+
+USE zsnare70;
+ALTER TABLE `categories` RENAME TO `Categories`;
+ALTER TABLE `customers` RENAME TO `Customers`;
+ALTER TABLE `order details` RENAME TO `OrderDetails`;
+ALTER TABLE `orders` RENAME TO `Orders`;
+ALTER TABLE `products` RENAME TO `Products`;
+ALTER TABLE `shippers` RENAME TO `Shippers`;
+ALTER TABLE `suppliers` RENAME TO `Suppliers`;
+
+USE tsnyder46;
+ALTER TABLE `categories` RENAME TO `Categories`;
+ALTER TABLE `customers` RENAME TO `Customers`;
+ALTER TABLE `order details` RENAME TO `OrderDetails`;
+ALTER TABLE `orders` RENAME TO `Orders`;
+ALTER TABLE `products` RENAME TO `Products`;
+ALTER TABLE `shippers` RENAME TO `Shippers`;
+ALTER TABLE `suppliers` RENAME TO `Suppliers`;
+
+USE ssteele31;
+ALTER TABLE `categories` RENAME TO `Categories`;
+ALTER TABLE `customers` RENAME TO `Customers`;
+ALTER TABLE `order details` RENAME TO `OrderDetails`;
+ALTER TABLE `orders` RENAME TO `Orders`;
+ALTER TABLE `products` RENAME TO `Products`;
+ALTER TABLE `shippers` RENAME TO `Shippers`;
+ALTER TABLE `suppliers` RENAME TO `Suppliers`;
+
+USE dstitt60;
+ALTER TABLE `categories` RENAME TO `Categories`;
+ALTER TABLE `customers` RENAME TO `Customers`;
+ALTER TABLE `order details` RENAME TO `OrderDetails`;
+ALTER TABLE `orders` RENAME TO `Orders`;
+ALTER TABLE `products` RENAME TO `Products`;
+ALTER TABLE `shippers` RENAME TO `Shippers`;
+ALTER TABLE `suppliers` RENAME TO `Suppliers`;
+
+USE nsuhoney56;
+ALTER TABLE `categories` RENAME TO `Categories`;
+ALTER TABLE `customers` RENAME TO `Customers`;
+ALTER TABLE `order details` RENAME TO `OrderDetails`;
+ALTER TABLE `orders` RENAME TO `Orders`;
+ALTER TABLE `products` RENAME TO `Products`;
+ALTER TABLE `shippers` RENAME TO `Shippers`;
+ALTER TABLE `suppliers` RENAME TO `Suppliers`;
+
+USE cwaite63;
+ALTER TABLE `categories` RENAME TO `Categories`;
+ALTER TABLE `customers` RENAME TO `Customers`;
+ALTER TABLE `order details` RENAME TO `OrderDetails`;
+ALTER TABLE `orders` RENAME TO `Orders`;
+ALTER TABLE `products` RENAME TO `Products`;
+ALTER TABLE `shippers` RENAME TO `Shippers`;
+ALTER TABLE `suppliers` RENAME TO `Suppliers`;
+
+USE zwillett62;
+ALTER TABLE `categories` RENAME TO `Categories`;
+ALTER TABLE `customers` RENAME TO `Customers`;
+ALTER TABLE `order details` RENAME TO `OrderDetails`;
+ALTER TABLE `orders` RENAME TO `Orders`;
+ALTER TABLE `products` RENAME TO `Products`;
+ALTER TABLE `shippers` RENAME TO `Shippers`;
+ALTER TABLE `suppliers` RENAME TO `Suppliers`;
+
+USE lwilt66;
+ALTER TABLE `categories` RENAME TO `Categories`;
+ALTER TABLE `customers` RENAME TO `Customers`;
+ALTER TABLE `order details` RENAME TO `OrderDetails`;
+ALTER TABLE `orders` RENAME TO `Orders`;
+ALTER TABLE `products` RENAME TO `Products`;
+ALTER TABLE `shippers` RENAME TO `Shippers`;
+ALTER TABLE `suppliers` RENAME TO `Suppliers`;
+
+USE mgermano79;
+ALTER TABLE `categories` RENAME TO `Categories`;
+ALTER TABLE `customers` RENAME TO `Customers`;
+ALTER TABLE `order details` RENAME TO `OrderDetails`;
+ALTER TABLE `orders` RENAME TO `Orders`;
+ALTER TABLE `products` RENAME TO `Products`;
+ALTER TABLE `shippers` RENAME TO `Shippers`;
+ALTER TABLE `suppliers` RENAME TO `Suppliers`;
